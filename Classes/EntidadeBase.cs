a5ec53044feff9c80f04ca56030a625ec736ff24
Lincoln Silva <Lincoln_Lima_Silva@outlook.com>
2022-01-07 23:16:24 -0300

    first commit

---
+++ Classes/EntidadeBase.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Proj_APP_Series
+{
+    public abstract class EntidadeBase
+    {
+        public int Id { get; protected set; }
+    }
+}
---
+++ Classes/Serie.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Proj_APP_Series
+{
+    public class Serie : EntidadeBase
+    {
+        private Genero Genero { get; set; }
+        private string Titulo { get; set; }
+
+        private string Descricao { get; set; }
+
+        private int Ano { get; set; }
+
+        private bool Excluido { get; set; }
+
+        public Serie(int id, Genero genero, string titulo, string descricao, int ano)
+        {
+            this.Id = id;
+            this.Genero = genero;
+            this.Titulo = titulo;
+            this.Descricao = descricao;
+            this.Ano = ano;
+            this.Excluido = false;
+
+        }
+
+        public override string ToString()
+        {
+            string retorno = "";
+            retorno += "Gênero: " + this.Genero + Environment.NewLine;
+            retorno += "Titulo: " + this.Titulo + Environment.NewLine;
+            retorno += "Descrição: " + this.Descricao + Environment.NewLine;
+            retorno += "Ano de Inicio: " + this.Ano;
+            return retorno;
+        }
+
+        public string retornaTitulo()
+        {
+            return this.Titulo;
+        }
+        internal int retornaId()
+        {
+            return this.Id;
+        }
+        public void Excluir()
+        {
+            this.Excluido = true;
+        }
+
+        internal object retornaExcluido()
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+
+
+
+}
